510bc1a56484e643ce7c37c087cc9c1e25694392
BibekBhusal0 <bibekbhusal04@gmail.com>
2025-07-24 23:07:24 +0545

    chore: setup project with npm and rollup
    - Initialize npm project and install rollup, plugins, and prettier
    - Configure rollup to bundle findbar-ai script
    - Add format script to run prettier for code formatting
    - Update github actions workflows to use npm and build scripts
    - Add .gitignore to ignore node_modules and dist directories

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { string } from 'rollup-plugin-string';
+
+const header = `// ==UserScript==
+// @name            BrowseBot
+// @description     Transforms the standard Zen Browser findbar into a modern, floating, AI-powered chat interface.
+// @author          BibekBhusal
+// ==/UserScript==
+
+`;
+
+export default {
+  input: 'findbar-ai/findbar-ai.uc.js',
+  output: [
+    {
+      file: 'dist/browse-bot.uc.js',
+      format: 'umd',
+      name: 'BrowseBot',
+      banner: header
+    }
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    string({
+      include: "**/*.css"
+    })
+  ]
+};
