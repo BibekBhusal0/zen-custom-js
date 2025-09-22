62cab21ca006c1fa0a29d4f0da84688274685034
BibekBhusal0 <bibekbhusal04@gmail.com>
2025-09-22 17:14:56 +0545

    refactor: remove utils/icons.js file

--- reopen-closed-tabs/utils/icon.js
+++
@@ -1,5 +0,0 @@
-export const icons = {
-  pinned: `chrome://browser/skin/zen-icons/pin.svg`,
-  essential: `chrome://browser/skin/zen-icons/essential-add.svg`,
-  folder: `chrome://browser/skin/folder.svg`,
-};
