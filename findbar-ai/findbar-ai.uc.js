fd2b6861b9d3d29464bd8c54230e527390b96c12
BibekBhusal0 <bibekbhusal04@gmail.com>
2025-10-13 23:31:20 +0545

    feat: highlight js to for syntax highlight for code

--- findbar-ai/findbar-ai.uc.js
+++ findbar-ai/findbar-ai.uc.js
@@ -3,8 +3,10 @@ import { browseBotFindbarLLM } from "./llm/index.js";
 import { PREFS, debugLog, debugError } from "./utils/prefs.js";
 import { parseElement, escapeXmlAttribute } from "./utils/parse.js";
 import { SettingsModal } from "./settings.js";
-import { marked } from "marked";
 import { toolNameMapping } from "./llm/tools.js";
+import { Marked } from "marked";
+import { markedHighlight } from "marked-highlight";
+import hljs from 'highlight.js';
 
 const icons = {
   loading: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="var(--browse-bot-muted)" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" width="100%" height="100%"><path d="M21 12a9 9 0 1 1-6.219-8.56"/></svg>`,
@@ -57,6 +59,17 @@ sidebarWidthUpdate();
 
 function parseMD(markdown, convertHTML = true) {
   const markedOptions = { breaks: true, gfm: true };
+  const marked = new Marked(
+    markedHighlight({
+      emptyLangClass: 'hljs',
+      langPrefix: 'hljs language-',
+      highlight(code, lang ) {
+        const language = hljs.getLanguage(lang) ? lang : 'auto';
+        return hljs.highlight(code, { language }).value;
+      }
+    })
+  );
+
   let content = marked.parse(markdown, markedOptions);
   content = content.replace(/<img([^>]*?)(?<!\/)>/gi, "<img$1 />")
     .replace(/<hr([^>]*?)(?<!\/)>/gi, "<hr$1 />")
