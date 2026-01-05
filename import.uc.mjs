51a5d4af90478417923d60ae5ddc14462eb495da
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-01-05 13:22:09 +0545

    copy some files from arcline

--- import.uc.mjs
+++ import.uc.mjs
@@ -9,6 +9,7 @@ import "./floating-sidebar/index.js";
 import "./reopen-closed-tabs/index.js";
 import "./search-engine-icon/index.js";
 import "./search-engine-select/index.js";
+import "./otehrs/tab-explode.js"
 
 // Make sure to run npm run Build before importing
 import "./dist/browse-bot-all.uc.js";
