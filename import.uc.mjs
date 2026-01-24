561f66efab291036cf621fe0d6dff348ff18d964
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-01-24 20:26:38 +0545

    resolving conflicts

--- import.uc.mjs
+++ import.uc.mjs
@@ -8,7 +8,6 @@
 import "./command-palette/index.js";
 import "./floating-sidebar/index.js";
 import "./reopen-closed-tabs/index.js";
-import "./search-engine-icon/index.js";
 import "./search-engine-select/index.js";
 import "./dist/browse-bot.uc.mjs";
 
