50b1cf08bd4d5649ce0e6667a33832e9456294c4
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-01-24 12:20:53 +0545

    feat: ✨ include bundled files of browsebot
    Installing everything with Sine works if i include it.

--- import.uc.mjs
+++ import.uc.mjs
@@ -4,15 +4,14 @@
 // @author          Bibek Bhusal
 // ==/UserScript==
 
+// My mods
 import "./command-palette/index.js";
 import "./floating-sidebar/index.js";
 import "./reopen-closed-tabs/index.js";
 import "./search-engine-icon/index.js";
 import "./search-engine-select/index.js";
+import "./dist/browse-bot-all.uc.js";
 
 // Otehr scripts
 import "./others/tab-explode.js";
 import "./others/invert-pdf.js";
-
-// Make sure to run npm run Build before importing
-import "./dist/browse-bot-all.uc.js";
