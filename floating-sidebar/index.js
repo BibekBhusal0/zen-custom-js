90b86eaa0dd9068c9bfcbab767e60536e9f310c1
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-02-06 21:23:19 +0545

    refactor: ♻️ remove not used icons
    this is done to reduce bundle size. In floating sidebar icons are not imported used icons are defined right there

--- floating-sidebar/index.js
+++ floating-sidebar/index.js
@@ -1,8 +1,12 @@
 import { startupFinish } from "../utils/startup-finish.js";
 import { parseElement, escapeXmlAttribute } from "../utils/parse.js";
-import { svgToUrl, icons } from "../utils/icon.js";
+import { svgToUrl } from "../utils/icon.js";
 import { addPrefListener, getPref, setPref } from "../utils/pref.js";
 
+const icons = {
+  pin: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" stroke="context-fill light-dark(black, white)" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 17v5M9 10.76a2 2 0 0 1-1.11 1.79l-1.78.9A2 2 0 0 0 5 15.24V16a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-.76a2 2 0 0 0-1.11-1.79l-1.78-.9A2 2 0 0 1 15 10.76V7a1 1 0 0 1 1-1a2 2 0 0 0 0-4H8a2 2 0 0 0 0 4a1 1 0 0 1 1 1z"/></svg>`,
+  unpin: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" stroke="context-fill light-dark(black, white)" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 17v5m3-12.66V7a1 1 0 0 1 1-1a2 2 0 0 0 0-4H7.89M2 2l20 20M9 9v1.76a2 2 0 0 1-1.11 1.79l-1.78.9A2 2 0 0 0 5 15.24V16a1 1 0 0 0 1 1h11"/></svg>`,
+};
 function addButton() {
   if (document.getElementById("sidebar-pin-unpin")) return;
 
