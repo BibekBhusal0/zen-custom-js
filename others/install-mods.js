d8a0dcd8571c404f135603fd1b42b16be9ccab18
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-06-21 23:35:44 +0545

    fix: 🐛 mod install script

--- others/install-mods.js
+++ others/install-mods.js
@@ -1,14 +1,15 @@
 import { startupFinish } from "../utils/startup-finish.js";
 
-startupFinish(() => {
+startupFinish(async () => {
   const mods_to_install = [
     "https://github.com/sameerasw/zen-themes/tree/main/TransparentZen",
     "https://github.com/Starry-AXQG/Context-Menu-Icons",
   ];
 
-  if (!window.SineAPI) {
+  if (window.SineAPI) {
     for (const mod of mods_to_install) {
-      SineAPI.manager.installMod(mod.homepage);
+      await new Promise((r) => setTimeout(r, 2000));
+      SineAPI.manager.installMod(mod);
     }
   }
 });
